638078b7724eae8bb70052c5839d4179791087df
unknown <cloverdev88@gmail.com>
2022-04-26 11:22:11 -0400

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,9 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
+require("@openzeppelin/hardhat-upgrades");
 const { mnemonic } = require('./secrets.json');
-const { BSC_API_KEY } = process.env;
+const { BSC_API_KEY, API_URL_ROPSTEN, API_URL_RINKEBY, PRIVATE_KEY, API_KEY } = process.env;
 
 
 // You need to export an object to set up your config
@@ -34,12 +35,25 @@ module.exports = {
          chainId: 56,
          gasPrice: 20000000000,
          accounts: { mnemonic: mnemonic }
+      },
+      ropsten: {
+         url: API_URL_ROPSTEN,
+         accounts: [`0x${PRIVATE_KEY}`]
+      },
+      rinkeby: {
+         url: API_URL_RINKEBY,
+         accounts: [`0x${PRIVATE_KEY}`]
       }
    },
    etherscan: {
       // Your API key for Etherscan
       // Obtain one at https://bscscan.com/
-      apiKey: BSC_API_KEY
+      apiKey: {
+         ropsten: API_KEY,
+         rinkeby: API_KEY,
+         testnet: BSC_API_KEY,
+         mainnet: BSC_API_KEY
+      }
    },
    solidity: {
       version: "0.8.0",
