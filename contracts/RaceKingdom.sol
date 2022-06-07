f526a809d555447b535b9b11f9d6d1181284f3cc
unknown <cloverdev88@gmail.com>
2022-06-07 17:57:44 -0400

    pull origin master

--- contracts/RaceKingdom.sol
+++ contracts/RaceKingdom.sol
@@ -209,7 +209,7 @@ contract RaceKingdom is Context, IBEP20, Ownable, ReentrancyGuard {
    */
   function _mint(address account, uint256 amount) internal {
     require(account != address(0), "BEP20: mint to the zero address");
-    require(_totalSupply.add(amount) <= MAX_SUPPLY, "This mint would exceed max supply of RKPOs");
+    require(_totalSupply.add(amount) <= MAX_SUPPLY, "This mint would exceed max supply of ATOZs");
 
     _totalSupply = _totalSupply.add(amount);
     _balances[account] = _balances[account].add(amount);
