7cbda0e6bdef01f8a8dc4cceff700e57ec4d1a8b
jewelas <jewelas.tm@gmail.com>
2022-12-29 08:25:57 -0500

    fix racekingdom

--- contracts/RaceKingdom.sol
+++ contracts/RaceKingdom.sol
@@ -3,16 +3,15 @@ pragma solidity 0.8.14;
 
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
 
 
 
-contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
+contract RaceKingdom is Context, IERC20, Ownable {
 
-  uint256 public constant MAX_SUPPLY = 3700000000000000000000000000;
+  uint256 public constant MAX_SUPPLY = 3_700_000_000_000_000_000_000_000_000;
 
   mapping (address => uint256) private _balances;
 
@@ -83,7 +82,7 @@ contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
    * - `recipient` cannot be the zero address.
    * - the caller must have a balance of at least `amount`.
    */
-  function transfer(address recipient, uint256 amount) external override nonReentrant returns (bool) {
+  function transfer(address recipient, uint256 amount) external override returns (bool) {
     _transfer(_msgSender(), recipient, amount);
     return true;
   }
@@ -91,8 +90,8 @@ contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
   /**
    * @dev See {BEP20-allowance}.
    */
-  function allowance(address owner, address spender) external view override returns (uint256) {
-    return _allowances[owner][spender];
+  function allowance(address holder, address spender) external view override returns (uint256) {
+    return _allowances[holder][spender];
   }
 
   /**
@@ -102,7 +101,7 @@ contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
    *
    * - `spender` cannot be the zero address.
    */
-  function approve(address spender, uint256 amount) external override nonReentrant returns (bool) {
+  function approve(address spender, uint256 amount) external override returns (bool) {
     _approve(_msgSender(), spender, amount);
     return true;
   }
@@ -119,7 +118,7 @@ contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
    * - the caller must have allowance for `sender`'s tokens of at least
    * `amount`.
    */
-  function transferFrom(address sender, address recipient, uint256 amount) external override nonReentrant returns (bool) {
+  function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
     _approve(sender, _msgSender(), _allowances[sender][_msgSender()] - amount);
     _transfer(sender, recipient, amount);
     return true;
@@ -137,7 +136,7 @@ contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
    *
    * - `spender` cannot be the zero address.
    */
-  function increaseAllowance(address spender, uint256 addedValue) public nonReentrant returns (bool) {
+  function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
     _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
     return true;
   }
@@ -156,7 +155,7 @@ contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
    * - `spender` must have allowance for the caller of at least
    * `subtractedValue`.
    */
-  function decreaseAllowance(address spender, uint256 subtractedValue) public nonReentrant returns (bool) {
+  function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
     _approve(_msgSender(), spender, _allowances[_msgSender()][spender] - subtractedValue);
     return true;
   }
@@ -169,7 +168,7 @@ contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
    *
    * - `msg.sender` must be the token owner
    */
-  function mint(address account, uint256 amount) public onlyOwner nonReentrant returns (bool) {
+  function mint(address account, uint256 amount) public onlyOwner returns (bool) {
     _mint(account, amount);
     return true;
   }
@@ -216,25 +215,6 @@ contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
   }
 
   /**
-   * @dev Destroys `amount` tokens from `account`, reducing the
-   * total supply.
-   *
-   * Emits a {Transfer} event with `to` set to the zero address.
-   *
-   * Requirements
-   *
-   * - `account` cannot be the zero address.
-   * - `account` must have at least `amount` tokens.
-   */
-  function _burn(address account, uint256 amount) internal {
-    require(account != address(0), "BEP20: burn from the zero address");
-
-    _balances[account] = _balances[account] - amount;
-    _totalSupply = _totalSupply - amount;
-    emit Transfer(account, address(0), amount);
-  }
-
-  /**
    * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
    *
    * This is internal function is equivalent to `approve`, and can be used to
@@ -247,22 +227,12 @@ contract RaceKingdom is Context, IERC20, Ownable, ReentrancyGuard {
    * - `owner` cannot be the zero address.
    * - `spender` cannot be the zero address.
    */
-  function _approve(address owner, address spender, uint256 amount) internal {
-    require(owner != address(0), "BEP20: approve from the zero address");
+  function _approve(address holder, address spender, uint256 amount) internal {
+    require(holder != address(0), "BEP20: approve from the zero address");
     require(spender != address(0), "BEP20: approve to the zero address");
 
-    _allowances[owner][spender] = amount;
-    emit Approval(owner, spender, amount);
+    _allowances[holder][spender] = amount;
+    emit Approval(holder, spender, amount);
   }
 
-  /**
-   * @dev Destroys `amount` tokens from `account`.`amount` is then deducted
-   * from the caller's allowance.
-   *
-   * See {_burn} and {_approve}.
-   */
-  function _burnFrom(address account, uint256 amount) internal {
-    _burn(account, amount);
-    _approve(account, _msgSender(), _allowances[account][_msgSender()] - amount);
-  }
 }
