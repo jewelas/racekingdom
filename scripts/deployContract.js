eccc71bfd3901740fb857f8747f38b15678a9f5b
jewelas <jewelas.tm@gmail.com>
2023-10-24 08:56:00 -0400

    add hot fox

--- scripts/deployContract.js
+++ scripts/deployContract.js
@@ -1,5 +1,5 @@
 async function main() {
-  const Contract = await ethers.getContractFactory("KingFox");
+  const Contract = await ethers.getContractFactory("HotFox");
 
   const contract = await Contract.deploy();
   await contract.deployed();
