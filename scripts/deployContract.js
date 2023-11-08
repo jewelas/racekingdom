2501ccb9629bb7bdd2a6f02c01bac9686fe07fb6
jewelas <jewelas.tm@gmail.com>
2023-11-08 07:54:16 -0500

    add black pink

--- scripts/deployContract.js
+++ scripts/deployContract.js
@@ -1,5 +1,5 @@
 async function main() {
-  const Contract = await ethers.getContractFactory("Bomb");
+  const Contract = await ethers.getContractFactory("BlackPink");
 
   const contract = await Contract.deploy();
   await contract.deployed();
