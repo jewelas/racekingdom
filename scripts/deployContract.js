69800d1828b265c31abd3471a02bc4070a60032b
jewelas <jewelas.tm@gmail.com>
2023-11-13 16:34:39 -0500

    add apple

--- scripts/deployContract.js
+++ scripts/deployContract.js
@@ -1,12 +1,12 @@
 async function main() {
-  const Contract = await ethers.getContractFactory("BlackPink");
+  const Contract = await ethers.getContractFactory("Apple");
 
   const contract = await Contract.deploy();
   await contract.deployed();
 
   console.log("Contract deployed to address:", contract.address); //0xbE3D68236A0402cDa2Bd78D2B24D47e5Ad3c1af3
 }
-
+//0x5E3DfD6Ed93B7093ab56CfE99449820f69777004
 main()
   .then(() => process.exit(0))
   .catch((error) => {
