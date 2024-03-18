d02ab7478e1f73252d84f274a0ade781574ba97c
jewelas <jewelas.tm@gmail.com>
2024-03-18 00:06:08 -0400

    fix config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,12 @@
 /**
-* @type import('hardhat/config').HardhatUserConfig
-*/
-require('dotenv').config();
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 // const { mnemonic } = require('./secrets.json');
-const { API_KEY, PRIVATE_KEY} = process.env;
-
+const { AVAX_API_KEY, PRIVATE_KEY } = process.env;
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,47 +15,48 @@ const { API_KEY, PRIVATE_KEY} = process.env;
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-   defaultNetwork: "hardhat",
-   networks: {
-      localhost: {
-         url: "http://127.0.0.1:8545"
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    hardhat: {
+      gas: "auto",
+    },
+    testnet: {
+      chainId: 43113,
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      // gasPrice: 20000000000,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mainnet: {
+      chainId: 43114,
+      url: "https://api.avax.network/ext/bc/C/rpc",
+
+      // gasPrice: 20000000000,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://bscscan.com/
+    apiKey: AVAX_API_KEY,
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
       },
-      hardhat: {
-         gas: "auto"
-      },
-      testnet: {
-         url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-         chainId: 5,
-         // gasPrice: 20000000000,
-         accounts: [`0x${PRIVATE_KEY}`]
-      },
-      mainnet: {
-         url: "https://mainnet.infura.io/v3/",
-         chainId: 1,
-         // gasPrice: 20000000000,
-         accounts: [`0x${PRIVATE_KEY}`]
-      }
-   },
-   etherscan: {
-      // Your API key for Etherscan
-      // Obtain one at https://bscscan.com/
-      apiKey: API_KEY
-   },
-   solidity: {
-      version: "0.8.19",
-      settings: {
-         optimizer: {
-            enabled: true
-         }
-      }
-   },
-   paths: {
-      sources: "./contracts",
-      tests: "./test",
-      cache: "./cache",
-      artifacts: "./artifacts"
-   },
-   mocha: {
-      timeout: 999999
-   }
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 999999,
+  },
 };
