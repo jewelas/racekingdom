73d1fb5ae750714773326d7db37d6920011d0787
jewelas <jewelas.tm@gmail.com>
2024-04-03 12:44:07 -0400

    seaGate

--- scripts/deployContract.js
+++ scripts/deployContract.js
@@ -1,17 +1,17 @@
 async function main() {
-  const Contract = await ethers.getContractFactory("Sample");
+  const Contract = await ethers.getContractFactory("SeaGateA");
   // const Contract = await ethers.getContractFactory("Bittenser");
 
   const contract = await Contract.deploy();
   await contract.deployed();
 
-  console.log("Contract deployed to address:", contract.address); //0x2Bca6B22970A200EbBF76cC69C7a23808967E378
-  //https://testnet.bscscan.com/address/0x2Bca6B22970A200EbBF76cC69C7a23808967E378#code
+  console.log("Contract deployed to address:", contract.address); //0xCc1e099beF74fc2788b1836F2460F70B9734cddF
+  //https://testnet.bscscan.com/address/0xCc1e099beF74fc2788b1836F2460F70B9734cddF#code
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch((error) => { 
     console.error(error);
     process.exit(1);
   });
